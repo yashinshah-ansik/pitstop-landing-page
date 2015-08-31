4421384f5bfd11cdb6b993ba17cc278d4c09faa3
Juodumas <juodumas@gmail.com>
2015-09-01 02:02:30 +0300

    initial upload

---
+++ dev/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+cd $(dirname "$0")
+
+distd=".."
+
+cat css/basscss/build/bass.css css/main.css css/bebasneue.css > ${distd}/bundle.css
+css="<style>$(cleancss --skip-import ${distd}/bundle.css)</style>"
+rm ${distd}/bundle.css
+
+cat <<EOF >${distd}/index.html
+<!doctype html>
+<html>
+<head>
+$(cat html/head.html)
+$css
+</head>
+<body>
+$(cat html/svg-icon-defs.svg)
+$(cat html/body.html)
+<script>$(cat js/outdated-browser.js)</script>
+</body>
+</html>
+EOF
+
